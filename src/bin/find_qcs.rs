91b8dd07d0e863af003fd697e2bbc7ff9537922a
Austin Platt <austinplatt@gmail.com>
2022-02-24 20:24:14 +0100

    use clap to parse arguments

--- src/bin/find_qcs.rs
+++ src/bin/find_qcs.rs
@@ -1,9 +1,25 @@
+use clap::Parser;
 use qc::generate::search::{find_solutions, print_solution};
 use qc::store::csv::get_clues;
 
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Size of the grid
+    #[clap(short, long, default_value_t = 3)]
+    size: usize,
+
+    /// Number of allowed non-surface words
+    #[clap(short, long, default_value_t = 0)]
+    allowed_missing_surfaces: usize,
+}
+
 fn main() {
+    let args = Args::parse();
+
     let clues = get_clues().unwrap();
-    let solutions = find_solutions(4, 2, clues);
+    let solutions = find_solutions(args.size, args.allowed_missing_surfaces, clues);
     for solution in solutions {
         print_solution(&solution);
         println!("~~~~~~~~");
