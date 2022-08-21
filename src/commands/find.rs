8a276667e4366c2e1242ca06021811e3f23cf7f4
Austin Platt <austinplatt@gmail.com>
2022-08-22 00:41:23 +0100

    add extra test case for one missing surface

--- src/commands/find.rs
+++ src/commands/find.rs
@@ -150,8 +150,45 @@ mod tests {
 
     use super::*;
 
+    fn run_test(
+        crossword_type: CrosswordType,
+        clues: Vec<(&str, &str)>,
+        expected_quines_list: Vec<QuinianCrossword>,
+        allowed_missing_surfaces: usize,
+    ) {
+        let clues_munged = clues
+            .into_iter()
+            .map(|(surface, answer)| return (surface.to_string(), answer.chars().collect()))
+            .collect();
+
+        let quines_mutex = Mutex::new(HashSet::new());
+
+        let store_quine = |crossword: &QuinianCrossword, _crossword_type: String, _score: usize| {
+            let mut quines = quines_mutex.lock().unwrap();
+            quines.insert(crossword.clone());
+        };
+        find_qcs(
+            clues_munged,
+            HashMap::new(),
+            crossword_type,
+            None,
+            0,
+            allowed_missing_surfaces,
+            &store_quine,
+        );
+
+        let mut expected_quines = HashSet::new();
+        for quine in expected_quines_list {
+            expected_quines.insert(quine.clone());
+        }
+
+        let quines = quines_mutex.lock().unwrap();
+        assert_eq!(quines.len(), expected_quines.len());
+        assert_eq!(expected_quines, *quines.deref());
+    }
+
     #[test]
-    fn simple_dense3() {
+    fn dense3_simple() {
         // Construct the clues so that it should be able to
         // make this crossword (and its transpose)
         // ABC   JKL
@@ -161,60 +198,21 @@ mod tests {
         // 2. A2     2. D2
         // 3. A3     3. D3
         let clues = vec![
-            ("A1".to_string(), vec!['A', 'B', 'C']),
-            ("A1".to_string(), vec!['J', 'K', 'L']),
-            ("A2".to_string(), vec!['D', 'E', 'F']),
-            ("A2".to_string(), vec!['M', 'N', 'O']),
-            ("A3".to_string(), vec!['G', 'H', 'I']),
-            ("A3".to_string(), vec!['P', 'Q', 'R']),
+            ("A1", "ABC"),
+            ("A1", "JKL"),
+            ("A2", "DEF"),
+            ("A2", "MNO"),
+            ("A3", "GHI"),
+            ("A3", "PQR"),
             // down clues
-            ("D1".to_string(), vec!['A', 'D', 'G']),
-            ("D1".to_string(), vec!['J', 'M', 'P']),
-            ("D2".to_string(), vec!['B', 'E', 'H']),
-            ("D2".to_string(), vec!['K', 'N', 'Q']),
-            ("D3".to_string(), vec!['C', 'F', 'I']),
-            ("D3".to_string(), vec!['L', 'O', 'R']),
+            ("D1", "ADG"),
+            ("D1", "JMP"),
+            ("D2", "BEH"),
+            ("D2", "KNQ"),
+            ("D3", "CFI"),
+            ("D3", "LOR"),
         ];
-
-        let quines_mutex = Mutex::new(HashSet::new());
-
-        let store_quine = |crossword: &QuinianCrossword, _crossword_type: String, _score: usize| {
-            let mut quines = quines_mutex.lock().unwrap();
-            quines.insert(crossword.clone());
-        };
-        find_qcs(
-            clues,
-            HashMap::new(),
-            CrosswordType::Dense3,
-            None,
-            0,
-            0,
-            &store_quine,
-        );
-
-        let expected_quines_list = vec![
-            QuinianCrossword {
-                grid1: vec![
-                    vec!['A', 'D', 'G'],
-                    vec!['B', 'E', 'H'],
-                    vec!['C', 'F', 'I'],
-                ],
-                grid2: vec![
-                    vec!['J', 'M', 'P'],
-                    vec!['K', 'N', 'Q'],
-                    vec!['L', 'O', 'R'],
-                ],
-                across_surfaces: vec![
-                    Some("D1".to_string()),
-                    Some("D2".to_string()),
-                    Some("D3".to_string()),
-                ],
-                down_surfaces: vec![
-                    Some("A1".to_string()),
-                    Some("A2".to_string()),
-                    Some("A3".to_string()),
-                ],
-            },
+        let expected_quines = vec![
             QuinianCrossword {
                 grid1: vec![
                     vec!['A', 'B', 'C'],
@@ -237,12 +235,85 @@ mod tests {
                     Some("D3".to_string()),
                 ],
             },
+            QuinianCrossword {
+                grid1: vec![
+                    vec!['A', 'D', 'G'],
+                    vec!['B', 'E', 'H'],
+                    vec!['C', 'F', 'I'],
+                ],
+                grid2: vec![
+                    vec!['J', 'M', 'P'],
+                    vec!['K', 'N', 'Q'],
+                    vec!['L', 'O', 'R'],
+                ],
+                across_surfaces: vec![
+                    Some("D1".to_string()),
+                    Some("D2".to_string()),
+                    Some("D3".to_string()),
+                ],
+                down_surfaces: vec![
+                    Some("A1".to_string()),
+                    Some("A2".to_string()),
+                    Some("A3".to_string()),
+                ],
+            },
         ];
-        let mut expected_quines = HashSet::new();
-        expected_quines.insert(expected_quines_list[0].clone());
-        expected_quines.insert(expected_quines_list[1].clone());
-        let quines = quines_mutex.lock().unwrap();
-        assert_eq!(quines.len(), expected_quines.len());
-        assert_eq!(expected_quines, *quines.deref());
+
+        run_test(CrosswordType::Dense3, clues, expected_quines, 0);
+    }
+
+    #[test]
+    fn dense3_one_missing() {
+        // Construct the clues so that it should be able to
+        // make this crossword
+        // ABC   JKL
+        // DEF   MNO
+        // GHI   PQR
+        // 1. A1          1. D1
+        // 2. A2          2. D2
+        // 3. [missing]   3. D3
+        let clues = vec![
+            ("A1", "ABC"),
+            ("A1", "JKL"),
+            ("A2", "DEF"),
+            ("A2", "MNO"),
+            // ("A3", "GHI"),
+            // ("A3", "PQR"),
+            // down clues
+            ("D1", "ADG"),
+            ("D1", "JMP"),
+            ("D2", "BEH"),
+            ("D2", "KNQ"),
+            ("D3", "CFI"),
+            ("D3", "LOR"),
+            // add in "GHI", and "PQR", so that we know
+            // the are real words
+            ("F1", "GHI"),
+            ("F2", "PQR"),
+        ];
+        let expected_quines = vec![QuinianCrossword {
+            grid1: vec![
+                vec!['A', 'B', 'C'],
+                vec!['D', 'E', 'F'],
+                vec!['G', 'H', 'I'],
+            ],
+            grid2: vec![
+                vec!['J', 'K', 'L'],
+                vec!['M', 'N', 'O'],
+                vec!['P', 'Q', 'R'],
+            ],
+            across_surfaces: vec![Some("A1".to_string()), Some("A2".to_string()), None],
+            down_surfaces: vec![
+                Some("D1".to_string()),
+                Some("D2".to_string()),
+                Some("D3".to_string()),
+            ],
+        }];
+
+        // if we dont allow one missing, then we wont find anything
+        run_test(CrosswordType::Dense3, clues.clone(), vec![], 0);
+        // but if we do allow one missing, we should only find the one
+        // possible qc
+        run_test(CrosswordType::Dense3, clues.clone(), expected_quines, 1);
     }
 }
